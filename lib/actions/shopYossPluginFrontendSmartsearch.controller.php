46163df8d805b8e90a60e7445552a7ce969c6757
Max Severin <makc.severin@gmail.com>
2015-02-04 23:39:23 +0300

    working with shopYossPluginFrontendSmartsearchController, getting brands/categories/products data

--- lib/actions/shopYossPluginFrontendSmartsearch.controller.php
+++ lib/actions/shopYossPluginFrontendSmartsearch.controller.php
@@ -13,6 +13,9 @@ class shopYossPluginFrontendSmartsearchController extends waJsonController
         if ( $settings['status'] ){
 
             $query = waRequest::post('query', '', waRequest::TYPE_STRING_TRIM);
+            $result = array();
+            $result['brands'] = array();
+            $result['categories'] = array();
             $result['products'] = array();
 
             $collection = new shopProductsCollection('search/query=' . $query);
@@ -21,21 +24,56 @@ class shopYossPluginFrontendSmartsearchController extends waJsonController
             if (!$product_limit) {
                 $product_limit = $this->getConfig()->getOption('products_per_page');
             }
-            $brand_limit = $settings['brandLimit'];
-            $category_limit = $settings['categoryLimit'];
 
             $products = $collection->getProducts('*', 0, $product_limit);
-            if ($products) {
+            
+            if ($products) {                
+                $brands = array();
+                $categories = array();
+                $brand_limit = $settings['brandLimit'];
+                $category_limit = $settings['categoryLimit'];
+                $feature_model = new shopFeatureModel();
                 foreach ($products as $p) {
+                    $brand_feature = $feature_model->getByCode('brand');
+                    if ($brand_feature) {
+                        $feature_value_model = $feature_model->getValuesModel($brand_feature['type']);
+                        $product_brands = $feature_value_model->getProductValues($p['id'], $brand_feature['id']);
+                        foreach ($product_brands as $k => $v) {
+                            $brands[$k] = $v;
+                        }   
+                    }                 
+                    $categories[] = $p['category_id'];
                     $result['products'][] = array(
                         "name" => $p['name'],
                         "url" => $p['frontend_url'],
                         "image" => ($p['image_id'] ? "<img src='" . shopImage::getUrl(array("product_id" => $p['id'], "id" => $p['image_id'], "ext" => $p['ext']), "48x48") . "' />" : ""),
-                        "price" => ($p['compare_price'] ? "<span class='compare-at-price'>".shop_currency($p['compare_price'], $p['currency'])."</span>" : "")."<span class='price'>".shop_currency($p['price'], $p['currency'])."</span>",
+                        
                     );
                 }
+                if ($brands) {      
+                    $brands = array_unique($brands); 
+                    foreach ($brands as $key => $value) {
+                        $result['brands'][] = array(
+                            "name" => $value,
+                            "url" => wa()->getRouteUrl('shop/frontend/brand', array('brand' => str_replace('%2F', '/', urlencode($value)))),
+                        );
+                    }
+                }
+                if ($categories) {
+                    $categories = array_unique($categories);
+                    $category_model = new shopCategoryModel();            
+                    foreach ($categories as $key => $value) {
+                        $category = $category_model->getById($value);
+                        if ($category) {
+                            $result['categories'][] = array(
+                                "name" => $category['name'],
+                                "url" => wa()->getRouteUrl('/frontend/category', array('category_url' => $category['full_url'])),
+                            );
+                        }
+                    }
+                }
             }
-
+            print_r($result['brands']); exit();
             $this->response = $result;
 
         } else {
