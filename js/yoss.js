fb6a7b9bc01f4fa5dc39117a219abad1268a8b3f
Max Severin <makc.severin@gmail.com>
2015-06-03 23:13:29 +0300

    yoss.js refactoring

--- js/yoss.js
+++ js/yoss.js
@@ -1,160 +1,155 @@
-$(document).ready(function() {
+function getProductBlock(product) {
+	var									
+		wrapperBlock, wrapperLeft, wrapperRight, productImg, productName, productBrands, productCategory, productPrice, productLink;
 
-	$("{$yoss_settings.id_in_html}").keyup(function() {
+	wrapperBlock    = $('<div/>').addClass("yoss-result-wrapper");
+	wrapperLeft     = $('<div/>').addClass("yoss-result-left");
+	wrapperRight    = $('<div/>').addClass("yoss-result-right");
 
-		var t = $(this);
+	productImg      = $('<a/>').attr("href", product.url).addClass("product-image").html(product.image);
+	productName     = $('<a/>').attr("href", product.url).addClass("product-name").html(product.name);
+	productBrands   = $('<div/>').addClass("product-brand");
+	productCategory = $('<div/>').addClass("product-category").html(product.category);
 
-		if ( t.val().length >= {$yoss_settings.min_char_count} ) {
+	productPrice    = $('<div/>').addClass("product-price").html(product.price);
+	productLink     = $("<a/>").addClass("product-link").attr("href", product.url).html("{_wp('to product')} &rarr;");
 
-			var resultBlock = $('<div/>').addClass("yoss-result loading");
+	if (product.brands.length > 0) {
+		for(var b in product.brands) {
+			productBrands = productBrands.append(product.brands[b].brand);
+		}
+	}
 
-			if ($(".yoss-result").length > 0) {
-				$(".yoss-result").remove();
-			} 
+	wrapperLeft.append(productImg, productName, productBrands, productCategory);
+	wrapperRight.append(productPrice, productLink);
+	wrapperBlock.append(wrapperLeft, wrapperRight);
 
-			t.addClass('active');
-			t.after(resultBlock);
+	return wrapperBlock
+}
 
-			$.ajax({
+function onResultScroll(event) {
+	var resultBlock = $(this);
+
+	if(resultBlock.scrollTop() + resultBlock.innerHeight() >= this.scrollHeight) {
+
+		var query = $("{$yoss_settings.id_in_html}").val();
+        var nextPage = resultBlock.find("#next_page").val();
+        var loadingBlock = $('<div/>').addClass("yoss-result-wrapper loading");
+        var lastEl = resultBlock.find('.yoss-result-wrapper:last-child');
+
+        if (query.length > 0 && nextPage > 0 ) {
+        	lastEl.after(loadingBlock);
+
+            $.ajax({
 				type: "POST",
 				url: "{$search_url}",
-				data: "query="+t.val()+"&pg=1",
+				data: "query="+query+"&pg="+nextPage,
 				success: function(response){
 
 					var result = $.parseJSON(response);
 
-					if (result.status === 'ok') {
+					$(".yoss-result-wrapper.loading").remove();
 
-						resultBlock.removeClass("loading");
+					if (result.status === 'ok' && result.data.products.length > 0) {
+						
 
-						if (result.data.products.length > 0) {
-
-							if (result.data.next_page !== false) {
-								var nextPage = $('<input/>').attr('type', 'hidden').attr('id', 'next_page').val(result.data.next_page);
-							} else {
-								var nextPage = $('<input/>').attr('type', 'hidden').attr('id', 'next_page').val('0');
-							}
-							resultBlock.append(nextPage);
-
-							var wrapperBlock = $('<div/>').addClass("yoss-result-wrapper");
-							var productCountSpan = $('<span/>').addClass("yoss-result-product-count").html("{_wp('Products found')}: " + result.data.product_count);
-							var searchAllUrlLink = $("<a/>").addClass("yoss-result-show-all").attr("href", result.data.searh_all_url).html("{_wp('to results')}");
-
-							wrapperBlock.append(productCountSpan, searchAllUrlLink);
-							resultBlock.append(wrapperBlock);
-
-							for(var key in result.data.products) {
-								var wrapperBlock = $('<div/>').addClass("yoss-result-wrapper");
-								var wrapperLeft = $('<div/>').addClass("yoss-result-left");
-								var wrapperRight = $('<div/>').addClass("yoss-result-right");
-
-								var productImg = $('<a/>').attr("href", result.data.products[key].url).addClass("product-image").html(result.data.products[key].image);
-								var productName = $('<a/>').attr("href", result.data.products[key].url).addClass("product-name").html(result.data.products[key].name);
-
-								var productBrands = $('<div/>').addClass("product-brand");
-								if (result.data.products[key].brands.length > 0) {
-									for(var b in result.data.products[key].brands) {
-										productBrands = productBrands.append(result.data.products[key].brands[b].brand);
-									}
-								}
-								var productCategory = $('<div/>').addClass("product-category").html(result.data.products[key].category);
-								var productPrice = $('<div/>').addClass("product-price").html(result.data.products[key].price);
-								var productLink = $("<a/>").addClass("product-link").attr("href", result.data.products[key].url).html("{_wp('to product')} &rarr;");
-
-								wrapperLeft.append(productImg, productName, productBrands, productCategory);
-								wrapperRight.append(productPrice, productLink);
-								wrapperBlock.append(wrapperLeft, wrapperRight);
-								resultBlock.append(wrapperBlock);
-							}
-
-
+						if (result.data.next_page !== false) {
+							resultBlock.find('#next_page').val(result.data.next_page);
 						} else {
-
-							resultBlock.addClass("no-products").html("{_wp('Sorry, but nothing was found, try to change your query')}");
-
+							resultBlock.find('#next_page').val('0');
 						}
+
+						for(var key in result.data.products) {
+							var getProductBlock = addProduct(result.data.products[key]);
+
+							lastEl.after(productBlock);
+						}	
+
 					}
 				}
 			}, 'json');
-			
-			{if $yoss_settings.lazy_loading === 'on'}
-			$(".yoss-result").scroll(function() {
-
-				var resBlock = $(this);
-
-				if(resBlock.scrollTop() + resBlock.innerHeight() >= this.scrollHeight) {
-
-					var query = $("{$yoss_settings.id_in_html}").val();
-		            var nextPage = resBlock.find("#next_page").val();
-		            var loadingBlock = $('<div/>').addClass("yoss-result-wrapper loading");
-		            var lastEl = resBlock.find('.yoss-result-wrapper:last-child');
-
-		            if (query.length > 0 && nextPage > 0 ) {
-		            	lastEl.after(loadingBlock);
-
-			            $.ajax({
-							type: "POST",
-							url: "{$search_url}",
-							data: "query="+query+"&pg="+nextPage,
-							success: function(response){
-
-								var result = $.parseJSON(response);
-
-								if (result.status === 'ok') {
-
-									$(".yoss-result-wrapper.loading").remove();
-
-									if (result.data.products.length > 0) {
-										if (result.data.next_page !== false) {
-											resBlock.find('#next_page').val(result.data.next_page);
-										} else {
-											resBlock.find('#next_page').val('0');
-										}
-
-										for(var key in result.data.products) {
-											var wrapperBlock = $('<div/>').addClass("yoss-result-wrapper");
-											var wrapperLeft = $('<div/>').addClass("yoss-result-left");
-											var wrapperRight = $('<div/>').addClass("yoss-result-right");
-
-											var productImg = $('<a/>').attr("href", result.data.products[key].url).addClass("product-image").html(result.data.products[key].image);
-											var productName = $('<a/>').attr("href", result.data.products[key].url).addClass("product-name").html(result.data.products[key].name);
-
-											var productBrands = $('<div/>').addClass("product-brand");
-											if (result.data.products[key].brands.length > 0) {
-												for(var b in result.data.products[key].brands) {
-													productBrands = productBrands.append(result.data.products[key].brands[b].brand);
-												}
-											}
-											var productCategory = $('<div/>').addClass("product-category").html(result.data.products[key].category);
-											var productPrice = $('<div/>').addClass("product-price").html(result.data.products[key].price);
-											var productLink = $("<a/>").addClass("product-link").attr("href", result.data.products[key].url).html('к товару &rarr;');
-
-											wrapperLeft.append(productImg, productName, productBrands, productCategory);
-											wrapperRight.append(productPrice, productLink);
-											wrapperBlock.append(wrapperLeft, wrapperRight);
-											lastEl.after(wrapperBlock);
-										}							
-
-									}
-
-								}
-							}
-						}, 'json');
-					}
-
-		        }
-
-			});
-			{/if}
-
-		} else {
-
-			t.removeClass('active');
-            $(".yoss-result").remove();
-			return false;
-
 		}
 
-	});
+    }
+}
+
+function onSearchKeyup(event) {
+	var t = $(this);
+
+	if ( t.val().length >= {$yoss_settings.min_char_count} ) {
+
+		var resultBlock = $('<div/>').addClass("yoss-result loading");
+
+		if ($(".yoss-result").length > 0) {
+			$(".yoss-result").remove();
+		} 
+
+		t.addClass('active');
+		t.after(resultBlock);
+
+		$.ajax({
+			type: "POST",
+			url: "{$search_url}",
+			data: "query="+t.val()+"&pg=1",
+			success: function(response){
+
+				var result = $.parseJSON(response);
+
+				resultBlock.removeClass("loading");
+
+				if (result.status === 'ok') {
+
+					if (result.data.products.length > 0) {
+
+						if (result.data.next_page !== false) {
+							var nextPage = $('<input/>').attr('type', 'hidden').attr('id', 'next_page').val(result.data.next_page);
+						} else {
+							var nextPage = $('<input/>').attr('type', 'hidden').attr('id', 'next_page').val('0');
+						}
+						resultBlock.append(nextPage);
+
+						var wrapperBlock = $('<div/>').addClass("yoss-result-wrapper");
+						var productCountSpan = $('<span/>').addClass("yoss-result-product-count").html("{_wp('Products found')}: " + result.data.product_count);
+						var searchAllUrlLink = $("<a/>").addClass("yoss-result-show-all").attr("href", result.data.searh_all_url).html("{_wp('to results')}");
+
+						wrapperBlock.append(productCountSpan, searchAllUrlLink);
+						resultBlock.append(wrapperBlock);
+
+						for(var key in result.data.products) {
+							var productBlock = getProductBlock(result.data.products[key]);
+
+							resultBlock.append(productBlock);
+						}
+
+
+					} else {
+
+						resultBlock.addClass("no-products").html("{_wp('Sorry, but nothing was found, try to change your query')}");
+
+					}
+				} else {
+
+					// error occured
+
+				}
+			}
+		}, 'json');
+		
+		{if $yoss_settings.lazy_loading === 'on'}
+		$(".yoss-result").scroll(onResultScroll);
+		{/if}
+
+	} else {
+
+		t.removeClass('active');
+        $(".yoss-result").remove();
+		return false;
+
+	}
+}
+
+$(document).ready(function() {
+
+	$("{$yoss_settings.id_in_html}").keyup(onSearchKeyup);
 
 });
