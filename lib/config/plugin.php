39240406337e8ecfe97f645261b2c57efda7fe3c
Max Severin <makc.severin@gmail.com>
2015-08-23 22:37:53 +0300

    version 1.0.1 init

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -8,7 +8,7 @@ return array(
     'description' => /*_wp*/('Product ajax-search with auto-loading'),
     'img' => 'img/yoss.png',
     'vendor' => 1020720,
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'shop_settings' => true,
     'frontend' => true,
     'handlers' => array(
