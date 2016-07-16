537b46d9d26d4617ac5de1c50e52ba5624f39c4f
Max Severin <makc.severin@gmail.com>
2016-07-16 21:22:21 +0300

    code review

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -8,7 +8,7 @@ return array(
     'description' => /*_wp*/('Product ajax-search with auto-loading'),
     'img' => 'img/yoss.png',
     'vendor' => 1020720,
-    'version' => '1.1.0',
+    'version' => '1.1.1',
     'shop_settings' => true,
     'custom_settings' => true,
     'frontend' => true,
